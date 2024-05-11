63a7585719b8d99babed6cf709a74b5f3c291c89
Tasneem Bholah <104076030+TasneemHUB@users.noreply.github.com>
2024-05-11 16:00:51 +0400

    Create contactcontent.js
    not yet ready! need to improve a bit more(details etc..)

---
+++ src/Components/contactcontent.js
@@ -0,0 +1,15 @@
+const contactConfig = {
+  YOUR_EMAIL: 'bismillah@gmail.com',
+  YOUR_phone: '(230) 58037784 / 57052441',
+  description:
+    'We appreciate your feedback! If you have any questions, comments, or suggestions for improvement, please do not hesitate to reach out to us.',
+
+};
+
+ 
+export {
+  
+
+  contactConfig, 
+
+};
